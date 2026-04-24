042150a1b823c538091514d9b0fe5f9c17238e8a
Kevien Damico <keviendamico@outlook.it>
2026-04-24 11:05:54 +0200

    Initial commit

---
+++ order-service/src/main/java/it/kevien/demo/orderservice/OrderServiceApplication.java
@@ -0,0 +1,10 @@
+package it.kevien.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderService {
+    public static void main(String[] args) {
+        System.out.println( "Hello World!" );
+    }
+}
